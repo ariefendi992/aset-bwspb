b5f0270895af9568e53e02c2feb5f88773d9b06c
sisdabwspb <sisdabwspb@gmail.com>
2026-05-16 01:02:04 +0900

    rm folder pengaman pantai

--- apps/pengaman_pantai/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class PengamanPantaiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.pengaman_pantai"
-    label = "pengamanPantai"
-    verbose_name = "Pengaman Pantai"
