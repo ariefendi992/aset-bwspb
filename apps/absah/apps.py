341a1bd333430141e4e428bbc591ade84e11fda7
sisdabwspb <sisdabwspb@gmail.com>
2026-05-16 00:58:03 +0900

    rm folder absah

--- apps/absah/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class AbsahConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.absah"
-    label = "absah"
-    verbose_name = "Data Absah"
