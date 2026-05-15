0397fcd7ec24074cf6446056b494a31eead8991b
sisdabwspb <sisdabwspb@gmail.com>
2026-05-16 01:03:16 +0900

    rm folder tanggul sungai

--- apps/tanggul_sungai/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class TanggulSungaiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.tanggul_sungai"
-    label = "tanggulSungai"
-    verbose_name = "Tanggul Sungai"
