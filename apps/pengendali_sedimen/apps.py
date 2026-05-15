75495df71b93e37a59a692e224e5448ca5aad99c
sisdabwspb <sisdabwspb@gmail.com>
2026-05-16 01:02:29 +0900

    rm folder pengendali sedimen

--- apps/pengendali_sedimen/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class PengendaliSedimenConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.pengendali_sedimen"
-    label = "pengendaliSedimen"
-    verbose_name = "Pengendali Sedimen"
