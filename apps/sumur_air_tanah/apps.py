99cbc28730dd0bc656440f1f46129f2d59f59973
sisdabwspb <sisdabwspb@gmail.com>
2026-05-16 01:02:56 +0900

    rm folder sumur air tanah

--- apps/sumur_air_tanah/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class SumurAirTanahConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.sumur_air_tanah"
-    label = "sumurAirTanah"
-    verbose_name = "Sumur Air Tanah"
